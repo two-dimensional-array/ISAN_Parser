ff37e9a0e4f852f12e648791df4015b4a264c561
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-17 13:40:45 +0300

    File ISANObject.h is updated.

---
+++ src/ISANObject.h
@@ -0,0 +1,25 @@
+#ifndef ISAN_OBJECT_H__
+#define ISAN_OBJECT_H__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+enum class ISANObjectType
+{
+  INT,
+  STRING,
+  ARRAY
+};
+
+class ISANObject
+{
+  public:
+    ISANObjectType type;
+    std::string value;
+    std::vector<std::shared_ptr<ISANObject>> items;
+    ISANObject get(int idx, int pathNum);
+};
+
+#endif //ISAN_OBJECT_H__
