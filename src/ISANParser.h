c33d6b6a379c09436fa4bd51804d65a02a3dc40f
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-17 15:04:35 +0300

    File ISANParser.h is updated.

---
+++ src/ISANParser.h
@@ -0,0 +1,32 @@
+#ifndef ISAN_PARSER_H__
+#define ISAN_PARSER_H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "ISANObject.h"
+
+enum class ISANTokenType
+{
+  OPEN_BRACKET,
+  CLOSE_BRACKET,
+  INT,
+  STRING,
+  COMMA,
+  OTHER
+};
+
+class ISANToken
+{   
+  public:
+		ISANTokenType type{ISANTokenType::OTHER};
+    std::string value;
+    int startPos;
+    int endPos;
+    ISANToken(ISANTokenType typ, std::string val, int start, int end);
+};
+
+ISANObject ParseFromTokens(std::vector<ISANToken> tokens);
+
+#endif // ISAN_PARSER_H__
